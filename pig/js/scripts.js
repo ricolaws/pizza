24bd9fe12feaad7aac5331315e0268240e9efa75
Richard Laws <rico@Richards-MacBook-Pro.local>
2018-10-23 21:12:26 -0700

    add accumulation of consequtive rolls into score, a roll of one resets score to zero.

--- pig/js/scripts.js
+++ pig/js/scripts.js
@@ -3,16 +3,36 @@ function rndNumber() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+function add(a, b) {
+    return a + b;
+}
+
+var turnScore = [];
+
 var player1 = {
   name: "Player 1",
-  turnScore: [],
   score: 0
+}
+
+function roll() {
+  if (x === 1) {
+    console.log("end turn");
+    turnScore = [0];
+  }
+  else {
+    turnScore.push(x);
+  }
 }
 
 $(document).ready(function() {
   $("form#player1Roll").submit(function(event) {
     event.preventDefault();
+    x = rndNumber();
+    roll();
+    $("#dice").text(x);
+    $("#player1-score-keeper").text(turnScore.reduce(add, 0));
 
-    $("#dice").text(rndNumber);
+
+    console.log(turnScore.reduce(add, 0));
   });
 });
