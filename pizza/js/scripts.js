cd580447814be4721e48ef3088b29a75612f9dfc
Richard Laws <rico@Richards-MacBook-Pro.local>
2018-10-25 02:45:54 -0700

    add pizza object on submit

--- pizza/js/scripts.js
+++ pizza/js/scripts.js
@@ -1,1 +1,19 @@
-console.log("a pizza");
+// Biz Logic
+function Pizza (size, toppings, values) {
+  this.size = size;
+  this.toppings = [];
+  this.values = [];
+}
+
+// Pizza.prototype.pizzaPrice = function() {
+//
+// };
+
+// User Logic
+$(document).ready(function() {
+  $("form.order-form").submit(function(event) {
+    event.preventDefault();
+    var pizza1 = new Pizza ("small", [],[]);
+    console.log(pizza1);
+  });
+});
