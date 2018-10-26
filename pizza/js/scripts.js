f8cf71d275b895f9aaa9356a318018a37ccdf705
Richard Laws <rico@Richards-MacBook-Pro.local>
2018-10-26 01:28:13 -0700

    add comments

--- pizza/js/scripts.js
+++ pizza/js/scripts.js
@@ -9,17 +9,24 @@ Pizza.prototype.pushTopping = function(x) {
   this.toppings.push(x)
 };
 
+function add(a, b) {
+    return a + b;
+}
+
 // User Logic
 $(document).ready(function() {
   $("form.order-form").submit(function(event) {
     event.preventDefault();
+    // new pizza object
     var pizza1 = new Pizza ("small", [],[]);
-    $("input:checkbox:checked").each(function(){
-    pizza1Values = $(this).val();
-    pizza1.values.push(pizza1Values)
-    });
+    // pushes the values from radios into the size array of the object
     $('input[name=pizza-size]:checked').each(function(){
     pizza1.size= $(this).val();
+    });
+    // pushes the values from checked boxes into the values array of the object
+    $("input:checkbox:checked").each(function(){
+    pizza1Values = parseInt($(this).val());
+    pizza1.values.push(pizza1Values)
     });
     $("input:checkbox[name=pepparoni]:checked").each(function(){
       var x = "pepparoni"
@@ -57,6 +64,9 @@ $(document).ready(function() {
       var x = "bacon-wrapped money"
       pizza1.pushTopping(x);
     });
+    item = pizza1.values;
+    $("#pizza-price").text(item.reduce(add, 0));
+    // console.log(getSum(item));
     console.log(pizza1);
   });
 });
